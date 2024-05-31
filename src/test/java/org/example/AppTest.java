e70dd54b108ba4e1916d3d52bb20e618030600ec
arfoysal <arfoysal.me@gmail.com>
2024-05-31 22:58:46 +0600

    added apk file and driver setup

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,22 @@
 package org.example;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.Key;
+import java.time.Duration;
 
 public class AppTest {
-    public static void main(String[] args) throws MalformedURLException {
+    public AndroidDriver driver;
+    @BeforeTest
+    public AndroidDriver startApp() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "android");
         desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
@@ -17,8 +27,27 @@ public class AppTest {
 
         URL remoteUrl = new URL("http://127.0.0.1:4723");
 
-        AndroidDriver driver = new AndroidDriver(remoteUrl, desiredCapabilities);
 
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+    }
+
+    @AfterTest
+    public void quitApp(){
         driver.quit();
     }
+    @Test
+    public void testApp() throws MalformedURLException, InterruptedException {
+        driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Angola']")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).clear();
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+        driver.hideKeyboard();
+        driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        Thread.sleep(5000);
+    }
+
 }
