d615b31571f49a53cd28746dacf9738c0f29b0e2
arfoysal <arfoysal.me@gmail.com>
2024-06-01 22:51:26 +0600

    Add app install, scroll and assertion

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.Key;
@@ -17,6 +21,8 @@ public class AppTest {
     public AndroidDriver driver;
     @BeforeTest
     public AndroidDriver startApp() throws MalformedURLException {
+        File f = new File("src/test/resources");
+        File APK = new File(f, "General-Store.apk");
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "android");
         desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
@@ -24,6 +30,7 @@ public class AppTest {
         desiredCapabilities.setCapability("appium:udid", "emulator-5554");
         desiredCapabilities.setCapability("appium:appPackage", "com.androidsample.generalstore");
         desiredCapabilities.setCapability("appium:appActivity", "com.androidsample.generalstore.SplashActivity");
+        desiredCapabilities.setCapability("appium:app", APK.getAbsolutePath());
 
         URL remoteUrl = new URL("http://127.0.0.1:4723");
 
@@ -40,13 +47,18 @@ public class AppTest {
     @Test
     public void testApp() throws MalformedURLException, InterruptedException {
         driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
-        driver.findElement(By.xpath("//android.widget.TextView[@text='Angola']")).click();
+        WebElement bd = driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text(\"Bangladesh\"))"));
+        bd.click();
+//        driver.findElement(By.xpath("//android.widget.TextView[@text='Angola']")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).clear();
         driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
         driver.hideKeyboard();
         driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Air Jordan 4 Retro']/following-sibling::android.widget.LinearLayout/android.widget.TextView[@text='ADD TO CART']")).click();
+        String cartItemCount = driver.findElement(By.id("com.androidsample.generalstore:id/counterText")).getText();
+        Assert.assertEquals(cartItemCount, "1");
         Thread.sleep(5000);
     }
 
